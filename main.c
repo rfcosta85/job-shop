63a695219312bfc241d365349b1afa1340c01bf4
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2024-04-23 15:26:19 +0100

    prototipo a funcionar

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <time.h>
 #include <omp.h>
 #include <stdbool.h>
+#include<unistd.h>
+
+#include <stdlib.h>
+
 #define MAX_NUM 25
 
 #ifdef _OPENMP
@@ -26,6 +30,7 @@ typedef struct  job {
 typedef  struct solution {
     int initialTime;
     int endTime;
+    int machine;
 } Solution;
 
 typedef struct  machine {
@@ -45,13 +50,15 @@ int main(int argc, char **argv) {
     int nrMachines;
     int nrOperations;
     int cost = 0;
-
+    
     inputFile = fopen(argv[1], "r");
     outputFile = fopen("output.txt", "w");
     fscanf(inputFile, "%d%d", &nrJobs, &nrMachines);
     nrOperations = nrMachines;
 
+    printf("nrJobs: %d\n", nrJobs);
     Solution solution[nrJobs][nrMachines];
+    memset(&solution, 0, sizeof(solution));
 
     printf("Jobs: %d\n", nrJobs);
     printf("Machines: %d\n", nrMachines);
@@ -83,7 +90,7 @@ int main(int argc, char **argv) {
     printf("\n\n\n");
     double initTime = getClock();
 
-    #pragma omp Parallel num_thread(nrJobs)
+    #pragma omp parallel num_threads(nrJobs)
     {
         int row = omp_get_thread_num();
 
@@ -94,6 +101,7 @@ int main(int argc, char **argv) {
                 printf("\nFree, row: %d", row);
 
                 solution[row][col].initialTime = maxEndTime;
+                solution[row][col].machine = jobs[row].operations[col].machine;
                 solution[row][col].endTime = solution[row][col].initialTime + jobs[row].operations[col].time;
 
                 machines[jobs[row].operations[col].machine].isFree = false;
@@ -102,6 +110,7 @@ int main(int argc, char **argv) {
                 printf("\nNot Free, row: %d", row);
 
                 solution[row][col].initialTime = machines[jobs[row].operations[col].machine].endTime;
+                solution[row][col].machine = jobs[row].operations[col].machine;
                 solution[row][col].endTime = solution[row][col].initialTime + jobs[row].operations[col].time;
 
                 machines[jobs[row].operations[col].machine].isFree = false;
@@ -133,8 +142,8 @@ int main(int argc, char **argv) {
         printf("Job %d | ", sr);
 
         for(int sc = 0; sc < nrMachines; sc ++) {
-            printf("id: %d e: %d | ", solution[sr][sc].initialTime, solution[sr][sc].endTime);
-            fprintf(outputFile, "%d", solution[sr][sc].initialTime);
+            printf("(%d)[%d,%d] | ", solution[sr][sc].machine ,solution[sr][sc].initialTime, solution[sr][sc].endTime);
+            fprintf(outputFile, "%d ", solution[sr][sc].initialTime);
         };
         printf("\n");
         fprintf(outputFile, "\n");
